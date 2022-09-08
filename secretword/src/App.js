4bab3eb9c76c2be6d0d68635ea740908891713eb
Michael <michaelpetterson06.com.br>
2022-09-08 10:45:18 -0300

    criando components  Game, GameOver  e importando -os junto com o words  no App

--- secretword/src/App.js
+++ secretword/src/App.js
@@ -1,10 +1,33 @@
 import './styles/App.css';
+
+
+import { useState } from 'react';
+import { wordsList } from './data/words';
+
+
 import StartScreen from './components/StartScreen.js';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages = [
+  {id: 1, name: 'start'},
+  {id: 2, name: 'game'},
+  {id: 3, name: 'end'}
+]
 
 function App() {
+    
+  const [gameStage] = useState(stages[0].name);
+  
+  const [words] = useState(wordsList);
+
+  console.log(words)
+
   return (
     <div className="App">
-      <StartScreen />
+      { gameStage === 'start' && <StartScreen />}    
+      { gameStage === 'game' && <Game />}
+      {gameStage === 'end' && <GameOver />}
     </div>
   );
 }
---
+++ secretword/src/components/Game.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Game.css';
+
+const Game = () => {
+  return (
+    <div>
+      Game
+    </div>
+  )
+}
+
+export default Game
